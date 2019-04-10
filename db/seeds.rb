a4c22f3267b3d44478249674cf022e35da2f6147
256hz <a.d@256hz.com>
2019-04-09 19:52:00 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "Zoe", email: "zoe@tdm.com")
+User.create(username: "Xanthia", email: "xanth@ert.com")
+User.create(username: "Yaria", email: "yari@mdc.com")
+
+Post.create(title: "I made a post", content: "It's about whales")
+Post.create(title: "I also posted", content: "This one is about birds")
+Post.create(title: "It's my post", content: "I posted about dodos in particular")
+
+Comment.create(content: "I liked it", user_id: 1, post_id: 1)
+Comment.create(content: "It was ok", user_id: 1, post_id: 2)
+Comment.create(content: "No like", user_id: 2, post_id: 2)
+Comment.create(content: "Undecided opinion", user_id: 3, post_id: 3)
